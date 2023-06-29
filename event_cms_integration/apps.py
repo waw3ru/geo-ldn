1b909ee7c453844cb2b7ce38641380a1eb39124e
waw3ru <waweruj00@gmail.com>
2023-06-29 20:58:47 +0300

    chore: Remove old website source files
    Setup latest django and wagtail

--- event_cms_integration/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventCmsIntegrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'event_cms_integration'
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geo_ldn.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
