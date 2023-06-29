c32a3dcaa9a5bc7c337b83ed6b743d93bf37b911
waw3ru <waweruj00@gmail.com>
2023-06-29 23:45:35 +0300

    chore: project settings and dependencies setup

--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+import dotenv
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geo_ldn.settings')
+
+    dotenv.read_dotenv()
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geo_ldn.settings")
 
     try:
         from django.core.management import execute_from_command_line
@@ -20,5 +26,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
