bdb4c4f93d6a77ac253df1fabbda71c09620e458
waw3ru <waweruj00@gmail.com>
2023-07-04 23:08:29 +0300

    fix: environment variables now available on wsgi and asgi

--- app/asgi.py
+++ app/asgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import dotenv
 
 from django.core.asgi import get_asgi_application
+
+dotenv.read_dotenv()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
