42dc399653c603cd08a491e202553562fb39a755
John ⚒ Waw3ru <waweruj00@gmail.com>
2023-07-04 21:02:27 +0300

    Update urls.py
    Signed-off-by: John ⚒ Waw3ru <waweruj00@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -11,10 +11,6 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
 ]
-
-if settings.DEBUG:
-    # urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
-    
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = urlpatterns + [
