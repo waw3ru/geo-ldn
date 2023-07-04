3530fd6449a9fa79bbff1458b147df9231b1423b
John ⚒ Waw3ru <waweruj00@gmail.com>
2023-07-04 20:57:13 +0300

    Update urls.py
    Signed-off-by: John ⚒ Waw3ru <waweruj00@gmail.com>

--- app/urls.py
+++ app/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
-
+    
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
