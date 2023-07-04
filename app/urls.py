163432c6573e565837de9da8b81bbc77f5e3e2c2
waw3ru <waweruj00@gmail.com>
2023-07-04 23:35:16 +0300

    fixes

--- app/urls.py
+++ app/urls.py
@@ -12,10 +12,11 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
 
 urlpatterns += [path("", include(wagtail_urls))]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
