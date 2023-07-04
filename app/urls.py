0eca37e79ec9ec5c82acdce8c7cc7cf9bcfac36c
waw3ru <waweruj00@gmail.com>
2023-07-05 01:56:40 +0300

    fixes

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
+import os
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import include, path
 from wagtail import urls as wagtail_urls
@@ -12,11 +15,30 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+
+if os.environ.get("IS_DOCKER", None) is not None:
+    urlpatterns += [
+        re_path(
+            r"^uploads/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.MEDIA_ROOT,
+            },
+        ),
+    ]
+
+    urlpatterns += [
+        re_path(
+            r"^public/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.STATIC_ROOT,
+            },
+        ),
+    ]
 
 urlpatterns += [path("", include(wagtail_urls))]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
